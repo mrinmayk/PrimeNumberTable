5a1f25c80a792cf20f85c1a6a5e84f7502518e1c
Mrinmay Kalita <mrinmay235@yahoo.com>
2018-03-16 23:26:03 -0700

    Added a few basic test cases

--- src/PrimeNumberTable.java
+++ src/PrimeNumberTable.java
@@ -8,38 +8,64 @@ class PrimeNumberTable
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	   int N=10;    // First 10 prime numbers
-	   int[] arr = new int[N+1]; /* To store the prime numbers and a 1 in [0][0] for aiding in multiplication for  in an array*/
-	   arr[0]=1;    
-	   int count=0; 
-	   int num=2;
-	   while(count!=N)// while count!= number of prime numbers entered keep searching.. 
-	   {
-	       boolean prime=true;// to determine whether the number is prime or not
-           for (int i=2;i<=Math.sqrt(num);i++)// most efficient is to check if number has any factor other than 1 starting from 1 to its square root
+	   table(N);
+	}
+	public static int[][] table(int N)
+	{
+		if(N<=0)
+		{
+			int[][] a = {{0}};
+			return a;
+		}
+			
+		int[] arr = new int[N+1]; /* To store the prime numbers and a 1 in an array. '1' helps to display primes in bottom rows by multiplying with the '1'. Eg 2X1 = 2 displayed on 2nd row beginning.*/
+		   arr[0]=1;    
+		   int count=0; 
+		   int num=2;
+		   while(count!=N)// while count!= number of prime numbers entered keep searching.. 
+		   {
+		       boolean prime=true;// to determine whether the number is prime or not
+	           for (int i=2;i<=Math.sqrt(num);i++)// most efficient is to check if number has any factor other than 1 starting from 1 to its square root
+		       {
+		           if (num%i==0)
+		           {
+		              prime=false;// if number divides any other number its not a prime so set prime to false and break the loop.
+		                break;
+		           }
+		       }
+		       if (prime)
+		       {
+		           count++;
+		           arr[count]=num;// store that num in the arr array
+		       }
+		       num++; //see if next number is prime or not.
+		   }
+		   
+		   int[][] a = new int[N+1][N+1];      // Results array; used for testing
+		   
+		   a[0][0] = 0;
+		   
+		   for(int i=1;i<N+1;i++)
+    	       	   a[0][i]=arr[i];
+		   
+		   for(int i=1;i<N+1;i++)
 	       {
-	           if (num%i==0)
-	           {
-	              prime=false;// if number divides any other number its not a prime so set prime to false and break the loop.
-	                break;
-	           }
-	       }
-	       if (prime)
-	       {
-	           count++;
-	           arr[count]=num;// store that num in the arr array
-	       }
-	       num++; //see if next number is prime or not.
-	   }
-	   System.out.printf("%5s"," ");           // Display 5 empty spaces on top left of table
+	           for(int j=0;j<N+1;j++)
+	                a[i][j]=arr[i]*arr[j];   // Multiple each prime by 1 (i.e. print itself) followed by other primes and display in a single row
+	       }		
+		   
+		   System.out.printf("%5s"," ");           // Display 5 empty spaces on top left of table
+
+		   for(int i=1;i<N+1;i++)
+    	       System.out.printf("%5d",a[0][i]);    // Printing the top row of table i.e. the list of the prime numbers
 	   
-	   for(int i=1;i<N+1;i++)
-    	       System.out.printf("%5d",arr[i]);    // Printing the top row of table i.e. the list of the prime numbers
-	   
-	   for(int i=1;i<N+1;i++)
-       {
-		   System.out.println("\n");
-           for(int j=0;j<N+1;j++)
-                System.out.printf("%5d",arr[i]*arr[j]);// Multiple each prime by 1 (i.e. print itself) followed by other primes and display in a single row
-       }
+	       for(int i=1;i<N+1;i++)
+           {
+		     System.out.println("\n");
+             for(int j=0;j<N+1;j++)
+                System.out.printf("%5d",a[i][j]);
+           }
+	       return a;
 	}
 }
+//Run Complexity of code is O(N^2) due to the nested for loop for doing the multiplication.
---
+++ src/PrimeNumberTableTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PrimeNumberTableTest extends PrimeNumberTable {
+
+	@Test
+	final void testMain1() {
+		int [][] arr= {
+			      {0, 2, 3, 5}, 
+			      {2, 4, 6, 10}, 
+			      {3, 6, 9, 15},
+			      {5, 10, 15, 25},};
+		Assertions.assertArrayEquals(arr,PrimeNumberTable.table(3));  // to test multiplication table for first 3 primes
+	}
+	
+	@Test
+	final void testMain2() {
+		int [][] arr= {
+			      {0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29}, 
+			      {2, 4, 6, 10, 14, 22, 26, 34, 38, 46, 58}, 
+			      {3, 6, 9, 15, 21, 33, 39, 51, 57, 69, 87},
+			      {5, 10, 15, 25, 35, 55, 65, 85, 95, 115, 145},
+				  {7, 14, 21, 35, 49, 77, 91, 119, 133, 161, 203}, 
+			      {11, 22, 33, 55, 77, 121, 143, 187, 209, 253, 319}, 
+			      {13, 26, 39, 65, 91, 143, 169, 221, 247, 299, 377},
+			      {17, 34, 51, 85, 119, 187, 221, 289, 323, 391, 493},
+			      {19, 38, 57, 95, 133, 209, 247, 323, 361, 437, 551},
+			      {23, 46, 69, 115, 161, 253, 299, 391, 437, 529, 667},
+			      {29, 58, 87, 145, 203, 319, 377, 493, 551, 667, 841},
+			}; 
+		Assertions.assertArrayEquals(arr,PrimeNumberTable.table(10));  // to test multiplication table for first 10 primes
+	}
+	
+	@Test
+	final void testMain3() {
+		int[][] arr = {{0}};
+		Assertions.assertArrayEquals(arr,PrimeNumberTable.table(0));  // to test for input less than or equal to 0
+	}
+}
